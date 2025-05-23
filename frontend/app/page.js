13e0a490090f2ab16b4f786ff6915396b49d4f33
gamelikehellboy <rrambhat@gitam.in>
2025-05-24 00:32:45 +0530

    minor layout fixes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,7 @@
 import ChatMain from "@/components/ChatMain";
-import InboxUI from "@/components/InboxUI";
 import SidebarLeft from "@/components/SidebarLeft";
 import SidebarRight from "@/components/SidebarRight";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
--- frontend/components/UserMessage.js
+++ frontend/components/UserMessage.js
@@ -2,20 +2,6 @@ import React from 'react'
 
 const UserMessage = ({message,user,profile_img,time,status}) => {
   return (
-    // <div className=" text-blue-900 text-xs rounded-lg p-3 max-w-max leading-tight relative">
-    //           <div className='pb-2'>
-    //             {user}
-    //           </div>
-    //           {message}
-    //           <div className="flex justify-end text-gray-600 text-[10px] font-medium mt-1">
-    //             <span>{status} · {time}</span>
-    //           </div>
-    //           <img
-    //             src={profile_img}
-    //             alt="Avatar of a woman with short hair"
-    //             className="absolute -left-6 top-[20px] -translate-y-1/2 rounded-full w-5 h-5"
-    //           />
-    // </div>
     <>
       {
         user === "You" ? (
